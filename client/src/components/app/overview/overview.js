5681b4662ab61adbbf62f0e81c6d10f7c852ef13
Bowen Brinegar <bowenbrinegar@gmail.com>
2018-02-11 18:28:20 -0500

    overview update in dev

--- client/src/components/app/overview/overview.js
+++ client/src/components/app/overview/overview.js
@@ -2,6 +2,10 @@ import React, { Component } from 'react'
 import "./overview.css"
 import BarChart from '../barchart/barchart';
 import DB from '../../../utils/DB'
+import LineChart from '../linechart/linechart';
+import csv from 'json2csv';
+import D3Line from '../d3Line/d3Line';
+
 
 const chart1 = {
   labels: ["Cushion", "Shoe", "Bed"],
@@ -75,6 +79,31 @@ const chart3 = {
   }
 };
 
+const chart4Line = {
+  labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+  datasets: [
+    {
+      label: 'My First dataset',
+      backgroundColor: "rgba(255,99,132,0.2)",
+      borderColor: "rgba(255,99,132,1)",
+      borderWidth: 1,
+      hoverBackgroundColor: "rgba(255,99,132,0.4)",
+      hoverBorderColor: "rgba(255,99,132,1)",
+      data: [65, 59, 80, 81, 56, 55, 40],
+    },
+    {
+      label: 'My Second dataset',
+      backgroundColor: "rgba(255,99,132,0.2)",
+      borderColor: "rgba(255,99,132,1)",
+      borderWidth: 1,
+      hoverBackgroundColor: "rgba(255,99,132,0.4)",
+      hoverBorderColor: "rgba(255,99,132,1)",
+      data: [28, 48, 40, 19, 86, 27, 90],
+    },
+  ]
+}
+
+
 class OverView extends Component {
   state = {
     institution: this.props.institution,
@@ -115,10 +144,16 @@ class OverView extends Component {
     this.setState({chart3: chart3})
   };
 
+  chart4 = () => {
+    this.setState({chart4: chart4Line})
+  }
+
+
   componentDidMount() {
     this.chart1();
     this.chart2();
     this.chart3();
+    this.chart4();
   }
 
   render() {
@@ -126,18 +161,16 @@ class OverView extends Component {
 
     return (
       <div className={this.props.container}>
-        {this.chart1 ? (<div id="chart1">
+        {this.chart1 !== {} ? (<div id="chart1">
           <BarChart data={this.state.chart1}/>
         </div>) : (<h1 id="chart1">No Data</h1>)}
-        {this.chart2 ? (<div id="chart2">
-          <BarChart data={this.state.chart2}/>
-        </div>) : (<h1 id="chart2">No Data</h1>)}
-        {this.chart3 ? (<div id="chart3">
-          <BarChart data={this.state.chart3}/>
-        </div>) : (<h1 id="chart3">No Data</h1>)}
-        {this.chart4 ? (<div id="chart4">
-          <BarChart data={this.state.chart4}/>
-        </div>) : (<h1 id="chart4">No Data</h1>)}
+        {/*{this.chart2 ? (<div id="chart2">*/}
+          {/*<BarChart data={this.state.chart2}/>*/}
+        {/*</div>) : (<h1 id="chart2">No Data</h1>)}*/}
+        {/*{this.chart3 ? (<div id="chart3">*/}
+          {/*<BarChart data={this.state.chart3}/>*/}
+        {/*</div>) : (<h1 id="chart3">No Data</h1>)}*/}
+        <div id='chart4'><D3Line/></div>
       </div>
     )
   }
