097c77e6352d62a9883e497d979678f26e53dcf3
Bowen Brinegar <bowenbrinegar@gmail.com>
2018-02-13 18:21:44 -0500

    sensor modal

--- client/src/components/app/modals/modal.js
+++ client/src/components/app/modals/modal.js
@@ -2,14 +2,39 @@ import React, { Component } from 'react'
 import "./modal.css"
 
 class Modal extends Component {
+  state = {
+    institution: '',
+
+  };
+
+  addSensor = () => {
+    console.log('success')
+  };
+
+  addData = () => {
+    console.log('success')
+  }
+
   render() {
     if (!this.props.show) {
       return null;
     } else if (this.props.value === "sensor") {
       return (
         <div id="modal">
-          <h1>Sensor Modal</h1>
-          <button value='Close' onClick={this.props.close}>Close</button>
+          <button id='Close'
+                  value='Close'
+                  onClick={this.props.close}>Close</button>
+          <div id='modalHeader'>
+            <h1 id='modalH1'>Sensor Modal</h1>
+          </div>
+          <div id='modalContent'>
+            <button id='sensorModalButton'
+                    onClick={this.addSensor}
+            >Add Sensor</button>
+            <button id='sensorModalButton'
+                    onClick={this.addData}
+            >Add Data</button>
+          </div>
         </div>
       )
     } else if (this.props.value === "institution") {
